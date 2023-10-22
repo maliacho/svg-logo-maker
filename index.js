3600aff0ae992c02228c4dec91834cdc58851507
maliacho <maliacho21@gmail.com>
2023-10-21 22:39:26 -0700

    continuing to update code so that it is functional. at the moment only the circle works.

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 // import necessary files and define global variables
 const inquirer = require('inquirer')
-const {Circle, Square, Triangle} = require('./lib/shapes')
+const { Circle, Square, Triangle } = require('./lib/shapes')
 const fs = require('fs');
 
 // Ask user for input on logo design
-const questions = [ 
+const questions = [
     {
         type: 'list',
-        message : 'What shape would you like your logo to be?',
-        name : 'shapeInput',
-        choices : [
+        message: 'What shape would you like your logo to be?',
+        name: 'shapeInput',
+        choices: [
             'Circle',
             'Square',
             'Triangle',
         ]
-    }
-    {
-        type : 'input',
-        message : 'What color would you like your logo to be?',
-        name : 'colorInput',
-    }
-    {
-        type : 'input',
-        message : 'What text would you like displayed on your logo?',
-        name : 'textInput',
-    }
+    },
     {
         type: 'input',
-        message : 'What color would you like the text to be?',
-        name : 'textColorInput',
+        message: 'What color would you like your logo to be?',
+        name: 'colorInput',
+    },
+    {
+        type: 'input',
+        message: 'What text would you like displayed on your logo?',
+        name: 'textInput',
+    },
+    {
+        type: 'input',
+        message: 'What color would you like the text to be?',
+        name: 'textColorInput',
     }
 ];
 
 
-function writeToFile (fileName, data) {
+function writeToFile(fileName, data) {
     fs.writeFile(`${fileName}.svg`, data, (err) => {
         err ? console.error(err) : console.log(`Your logo has been created.`)
     })
@@ -43,20 +43,19 @@ const init = async () => {
     const results = await inquirer.prompt(questions)
     //   .then(writeToFile)
     let shapeChoice;
-    if (shapeChoice === 'Square') {
-        shapeChoice = new Square();
-    } else if (shapeChoice === 'Circle') {
-        shapeChoice = new Circle;
-    } else if (shapeChoice === 'Triangle') {
-        shapeChoice = new Triangle;
+    if (results.shapeInput === 'Square') {
+        shapeChoice = new Square(results.colorInput, results.textInput, results.textColorInput);
+    } else if (results.shapeInput === 'Circle') {
+        shapeChoice = new Circle(results.colorInput, results.textInput, results.textColorInput);
+    } else if (results.shapeInput === 'Triangle') {
+        shapeChoice = new Triangle(results.colorInput, results.textInput, results.textColorInput);
     } else {
         console.log('Please select a shape from the list.')
     }
 
-    let shapeColor;
-    if (shapeColor === '')
+    writeToFile(`logo`, shapeChoice.display())
 
 
-  };
+};
 
-  init();
+init();
